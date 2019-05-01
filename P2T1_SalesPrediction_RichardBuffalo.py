c6b3ffa22bd0479cfbfc2ed6c125882e9b2421a4
TommyCTI110 <50179937+TommyCTI110@users.noreply.github.com>
2019-05-01 14:00:01 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_RichardBuffalo.py
@@ -0,0 +1,11 @@
+# In this project we'll be making a program that takes user inputs and converts pounds to kilograms
+# 04-01-2019
+# CTI-110 P2HW1 - Pounds to Kilograms Converter
+# Richard T Buffalo
+
+
+pounds = float(input('pounds of objects: '))
+
+pounds = pounds / 2.2046
+
+print("The pounds converted to kilograms is", format(pounds, ",.2f"))
